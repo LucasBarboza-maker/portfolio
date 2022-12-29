2b9f2696bb77993becc2aef628ed7434a1aed68a
LucasBarboza-maker <61573957+LucasBarboza-maker@users.noreply.github.com>
2022-12-29 10:34:23 -0300

    Revert "Chore: modiffying profile image"
    This reverts commit fbedc4f5a3145efd76a34a35f009240ba6eec740.

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,9 +14,7 @@ module.exports = {
       },
       backgroundImage: {
         'computer': "url('../public/images/computer_darker.jpg')",
-        'programming': "url('../public/images/programming_computer.jpg')",
-        'me': "url('../public/images/my_site_photo.jpg')"
-
+        'programming': "url('../public/images/programming_computer.jpg')"
       },
       height: {
         '90vh': '90vh',
